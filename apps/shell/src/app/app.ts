68ca099c758a5527a16a841b121340e49dddddec
10GO <mixelidzet@gmail.com>
2026-08-22 01:42:24 +0400

    styles

--- apps/shell/src/app/app.ts
+++ apps/shell/src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
 import { ThemeService } from '@t10go-design-system';
+import { MatDatepickerToggle } from '@angular/material/datepicker';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [NxWelcome, RouterModule, MatDatepickerToggle],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
