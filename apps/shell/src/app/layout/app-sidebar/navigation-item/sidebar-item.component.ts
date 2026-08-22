d25bf406e7524f41853a6e7546f69e787af5c988
lengi21 <mixelidzet@gmail.com>
2026-08-22 16:54:42 +0400

    fix(shell): enable recursive standalone sidebar item

--- apps/shell/src/app/layout/app-sidebar/navigation-item/sidebar-item.component.ts
+++ apps/shell/src/app/layout/app-sidebar/navigation-item/sidebar-item.component.ts
@@ -1,11 +1,11 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, forwardRef, input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 import { NavigationItem } from '../../../../shared';
 
 @Component({
   selector: 'app-sidebar-item',
-  imports: [RouterLink, MatIconModule],
+  imports: [RouterLink, MatIconModule, forwardRef(() => SidebarItem)],
   templateUrl: './sidebar-item.component.html',
   styleUrl: './sidebar-item.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
