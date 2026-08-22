c23607b08ce516baa70c80de4aeee3336a7ab54d
lengi21 <mixelidzet@gmail.com>
2026-08-22 16:54:23 +0400

    refactor(shell): simplify sidebar component dependencies

--- apps/shell/src/app/layout/app-sidebar/app-sidebar.ts
+++ apps/shell/src/app/layout/app-sidebar/app-sidebar.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
 import { NAVIGATION_ITEMS } from './config/navigation.config';
-import { RouterLink } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
 import { SidebarItem } from './navigation-item/sidebar-item.component';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink, MatIconModule, SidebarItem],
+  imports: [SidebarItem],
   templateUrl: './app-sidebar.html',
   styleUrl: './app-sidebar.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppSidebar {
   readonly navigationItems = NAVIGATION_ITEMS;
